fa75aef224a5c1158530a940ca282c9b4b708228
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2020-06-08 07:47:57 -0700

    copying code
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

---
+++ samples/src/test/java/io/pravega/schemaregistry/samples/PassthruSchemaRegistryClient.java
@@ -0,0 +1,171 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.schemaregistry.samples;
+
+import io.pravega.schemaregistry.MapWithToken;
+import io.pravega.schemaregistry.client.SchemaRegistryClient;
+import io.pravega.schemaregistry.client.exceptions.RegistryExceptions;
+import io.pravega.schemaregistry.common.ContinuationTokenIterator;
+import io.pravega.schemaregistry.contract.data.EncodingId;
+import io.pravega.schemaregistry.contract.data.EncodingInfo;
+import io.pravega.schemaregistry.contract.data.GroupHistoryRecord;
+import io.pravega.schemaregistry.contract.data.GroupProperties;
+import io.pravega.schemaregistry.contract.data.SchemaInfo;
+import io.pravega.schemaregistry.contract.data.SchemaValidationRules;
+import io.pravega.schemaregistry.contract.data.SchemaWithVersion;
+import io.pravega.schemaregistry.contract.data.VersionInfo;
+import io.pravega.schemaregistry.service.SchemaRegistryService;
+import io.pravega.schemaregistry.storage.ContinuationToken;
+
+import javax.annotation.Nullable;
+import java.util.AbstractMap;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class PassthruSchemaRegistryClient implements SchemaRegistryClient {
+    private final SchemaRegistryService service;
+    private final String namespace;
+
+    public PassthruSchemaRegistryClient(SchemaRegistryService service, String namespace) {
+        this.service = service;
+        this.namespace = namespace;
+    }
+
+    public PassthruSchemaRegistryClient(SchemaRegistryService service) {
+        this(service, null);
+    }
+
+    @Override
+    public boolean addGroup(String group, GroupProperties properties) {
+        return service.createGroup(namespace, group, properties).join();
+    }
+
+    @Override
+    public void removeGroup(String group) {
+        service.deleteGroup(namespace, group).join();
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, GroupProperties>> listGroups() {
+        Function<ContinuationToken, Map.Entry<ContinuationToken, Collection<Map.Entry<String, GroupProperties>>>> function =
+                token -> {
+                    MapWithToken<String, GroupProperties> result = service.listGroups(namespace, token, 100).join();
+                    return new AbstractMap.SimpleEntry<>(result.getToken(), result.getMap().entrySet());
+                };
+        return new ContinuationTokenIterator<>(function, null);
+    }
+
+    @Override
+    public GroupProperties getGroupProperties(String group) {
+        return service.getGroupProperties(namespace, group).join();
+    }
+
+    @Override
+    public boolean updateSchemaValidationRules(String group, SchemaValidationRules validationRules, SchemaValidationRules previousRules) {
+        return service.updateSchemaValidationRules(namespace, group, validationRules, previousRules)
+                      .handle((r, e) -> e != null).join();
+    }
+
+    @Override
+    public List<SchemaWithVersion> getSchemas(String group) {
+        return service.getSchemas(namespace, group, null).join();
+    }
+
+    @Override
+    public VersionInfo addSchema(String group, SchemaInfo schemaInfo) {
+        return service.addSchema(namespace, group, schemaInfo).join();
+    }
+
+    @Override
+    public void deleteSchemaVersion(String group, VersionInfo versionInfo) {
+        service.deleteSchema(namespace, group, versionInfo.getOrdinal()).join();
+    }
+
+    @Override
+    public void deleteSchemaVersion(String group, String schemaType, int version) {
+        service.deleteSchema(group, schemaType, version).join();
+    }
+
+    @Override
+    public SchemaInfo getSchemaForVersion(String group, VersionInfo versionInfo) {
+        return service.getSchema(namespace, group, versionInfo.getOrdinal()).join();
+    }
+
+    @Override
+    public SchemaInfo getSchemaForVersion(String group, String schemaType, int version) {
+        return service.getSchema(namespace, group, schemaType, version).join();
+    }
+
+    @Override
+    public EncodingInfo getEncodingInfo(String group, EncodingId encodingId) {
+        return service.getEncodingInfo(namespace, group, encodingId).join();
+    }
+
+    @Override
+    public EncodingId getEncodingId(String group, VersionInfo versionInfo, String codecType) {
+        return service.getEncodingId(namespace, group, versionInfo, codecType).join();
+    }
+
+    @Override
+    public SchemaWithVersion getLatestSchemaVersion(String group, @Nullable String schemaType) {
+        List<SchemaWithVersion> latestSchemas = service.getSchemas(namespace, group, schemaType).join();
+        if (schemaType == null) {
+            return latestSchemas.stream().max(Comparator.comparingInt(x -> x.getVersionInfo().getOrdinal())).orElse(null);
+        } else {
+            return latestSchemas.get(0);
+        }
+    }
+
+    @Override
+    public List<SchemaWithVersion> getSchemaVersions(String group, @Nullable String schemaType) {
+        return service.getGroupHistory(namespace, group, schemaType)
+                      .thenApply(history -> history.stream().map(x -> new SchemaWithVersion(x.getSchema(), x.getVersion())).collect(Collectors.toList())).join();
+    }
+
+    @Override
+    public List<GroupHistoryRecord> getGroupHistory(String group) {
+        return service.getGroupHistory(namespace, group, null).join();
+    }
+
+    @Override
+    public Map<String, VersionInfo> getSchemaReferences(SchemaInfo schemaInfo) throws RegistryExceptions.ResourceNotFoundException, RegistryExceptions.UnauthorizedException {
+        return service.getSchemaReferences(namespace, schemaInfo).join();
+    }
+
+    @Override
+    public VersionInfo getVersionForSchema(String group, SchemaInfo schemaInfo) {
+        return service.getSchemaVersion(namespace, group, schemaInfo).join();
+    }
+
+    @Override
+    public boolean validateSchema(String group, SchemaInfo schemaInfo) {
+        return service.validateSchema(namespace, group, schemaInfo).join();
+    }
+
+    @Override
+    public boolean canReadUsing(String group, SchemaInfo schemaInfo) {
+        return service.canRead(namespace, group, schemaInfo).join();
+    }
+
+    @Override
+    public List<String> getCodecTypes(String group) {
+        return service.getCodecTypes(namespace, group).join();
+    }
+
+    @Override
+    public void addCodecType(String group, String codecType) {
+        service.addCodecType(namespace, group, codecType).join();
+    }
+}
---
+++ samples/src/test/java/io/pravega/schemaregistry/samples/TestEndToEnd.java
@@ -0,0 +1,213 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.schemaregistry.samples;
+
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Lists;
+import io.pravega.common.Exceptions;
+import io.pravega.schemaregistry.client.SchemaRegistryClient;
+import io.pravega.schemaregistry.codec.CodecFactory;
+import io.pravega.schemaregistry.contract.data.Compatibility;
+import io.pravega.schemaregistry.contract.data.EncodingId;
+import io.pravega.schemaregistry.contract.data.GroupHistoryRecord;
+import io.pravega.schemaregistry.contract.data.GroupProperties;
+import io.pravega.schemaregistry.contract.data.SchemaInfo;
+import io.pravega.schemaregistry.contract.data.SchemaValidationRules;
+import io.pravega.schemaregistry.contract.data.SchemaWithVersion;
+import io.pravega.schemaregistry.contract.data.SerializationFormat;
+import io.pravega.schemaregistry.contract.data.VersionInfo;
+import io.pravega.schemaregistry.exceptions.IncompatibleSchemaException;
+import io.pravega.schemaregistry.service.SchemaRegistryService;
+import io.pravega.schemaregistry.storage.SchemaStore;
+import io.pravega.schemaregistry.storage.StoreExceptions;
+import io.pravega.test.common.AssertExtensions;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.curator.shaded.com.google.common.base.Charsets;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@Slf4j
+public abstract class TestEndToEnd {
+    @Rule
+    public Timeout globalTimeout = new Timeout(3, TimeUnit.MINUTES);
+
+    ScheduledExecutorService executor;
+
+    private final Schema schema1 = SchemaBuilder
+            .record("MyTest")
+            .fields()
+            .name("a")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .endRecord();
+
+    private final Schema schema2 = SchemaBuilder
+            .record("MyTest")
+            .fields()
+            .name("a")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .name("b")
+            .type(Schema.create(Schema.Type.STRING))
+            .withDefault("backward compatible with schema1")
+            .endRecord();
+
+    private final Schema schema3 = SchemaBuilder
+            .record("MyTest")
+            .fields()
+            .name("a")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .name("b")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .endRecord();
+
+    private final Schema schemaTest2 = SchemaBuilder
+            .record("MyTest2")
+            .fields()
+            .name("a")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .name("b")
+            .type(Schema.create(Schema.Type.STRING))
+            .noDefault()
+            .endRecord();
+    
+    @Before
+    public void setUp() {
+        executor = Executors.newScheduledThreadPool(10);
+    }
+
+    @After
+    public void tearDown() {
+        executor.shutdownNow();
+    }
+    
+    @Test
+    public void testEndToEnd() {
+        SchemaStore store = getStore();
+        SchemaRegistryService service = new SchemaRegistryService(store, executor);
+        SchemaRegistryClient client = new PassthruSchemaRegistryClient(service);
+
+        String group = "group";
+
+        int groupsCount = Lists.newArrayList(client.listGroups()).size();
+
+        client.addGroup(group, new GroupProperties(SerializationFormat.Avro,
+                SchemaValidationRules.of(Compatibility.backward()),
+                true));
+        assertEquals(Lists.newArrayList(client.listGroups()).size(), groupsCount + 1);
+
+        String myTest = "MyTest";
+        SchemaInfo schemaInfo = new SchemaInfo(myTest, SerializationFormat.Avro,
+                ByteBuffer.wrap(schema1.toString().getBytes(Charsets.UTF_8)), ImmutableMap.of());
+
+        VersionInfo version1 = client.addSchema(group, schemaInfo);
+        assertEquals(version1.getVersion(), 0);
+        assertEquals(version1.getOrdinal(), 0);
+        assertEquals(version1.getType(), myTest);
+        // attempt to add an existing schema
+        version1 = client.addSchema(group, schemaInfo);
+        assertEquals(version1.getVersion(), 0);
+        assertEquals(version1.getOrdinal(), 0);
+        assertEquals(version1.getType(), myTest);
+
+        SchemaInfo schemaInfo2 = new SchemaInfo(myTest, SerializationFormat.Avro,
+                ByteBuffer.wrap(schema2.toString().getBytes(Charsets.UTF_8)), ImmutableMap.of());
+        VersionInfo version2 = client.addSchema(group, schemaInfo2);
+        assertEquals(version2.getVersion(), 1);
+        assertEquals(version2.getOrdinal(), 1);
+        assertEquals(version2.getType(), myTest);
+
+        client.updateSchemaValidationRules(group, SchemaValidationRules.of(Compatibility.fullTransitive()), null);
+
+        SchemaInfo schemaInfo3 = new SchemaInfo(myTest, SerializationFormat.Avro,
+                ByteBuffer.wrap(schema3.toString().getBytes(Charsets.UTF_8)), ImmutableMap.of());
+
+        AtomicReference<Throwable> exceptionRef = new AtomicReference<>();
+        CompletableFuture.supplyAsync(() -> client.addSchema(group, schemaInfo3))
+                         .exceptionally(e -> {
+                             exceptionRef.set(Exceptions.unwrap(e));
+                             return null;
+                         }).join();
+
+        assertTrue(exceptionRef.get() instanceof IncompatibleSchemaException);
+
+        String myTest2 = "MyTest2";
+        SchemaInfo schemaInfo4 = new SchemaInfo(myTest2, SerializationFormat.Avro,
+                ByteBuffer.wrap(schemaTest2.toString().getBytes(Charsets.UTF_8)), ImmutableMap.of());
+        VersionInfo version3 = client.addSchema(group, schemaInfo4);
+        assertEquals(version3.getVersion(), 0);
+        assertEquals(version3.getOrdinal(), 2);
+        assertEquals(version3.getType(), myTest2);
+
+        List<String> types = client.getSchemas(group).stream().map(x -> x.getSchemaInfo().getType()).collect(Collectors.toList());
+        assertEquals(types.size(), 2);
+        assertTrue(types.contains(myTest));
+        assertTrue(types.contains(myTest2));
+        List<GroupHistoryRecord> groupEvolutionHistory = client.getGroupHistory(group);
+        assertEquals(groupEvolutionHistory.size(), 3);
+        List<SchemaWithVersion> myTestHistory = client.getSchemaVersions(group, myTest);
+        assertEquals(myTestHistory.size(), 2);
+        List<SchemaWithVersion> myTest2History = client.getSchemaVersions(group, myTest2);
+        assertEquals(myTest2History.size(), 1);
+
+        // delete schemainfo2
+        EncodingId encodingId = client.getEncodingId(group, version2, CodecFactory.NONE);
+        assertEquals(encodingId.getId(), 0);
+        client.deleteSchemaVersion(group, version2);
+        SchemaInfo schema = client.getSchemaForVersion(group, version2);
+        assertEquals(schema, schemaInfo2);
+        AssertExtensions.assertThrows("", () -> client.getVersionForSchema(group, schemaInfo2),
+                e -> Exceptions.unwrap(e) instanceof StoreExceptions.DataNotFoundException);
+        encodingId = client.getEncodingId(group, version2, CodecFactory.NONE);
+        assertEquals(encodingId.getId(), 0);
+        AssertExtensions.assertThrows("", () -> client.getEncodingId(group, version2, CodecFactory.MIME_GZIP),
+                e -> Exceptions.unwrap(e) instanceof StoreExceptions.DataNotFoundException);
+
+        groupEvolutionHistory = client.getGroupHistory(group);
+        assertEquals(groupEvolutionHistory.size(), 2);
+
+        myTestHistory = client.getSchemaVersions(group, myTest);
+        assertEquals(myTestHistory.size(), 1);
+        SchemaWithVersion schemaWithVersion = client.getLatestSchemaVersion(group, myTest);
+        assertEquals(schemaWithVersion.getVersionInfo(), version1);
+
+        schemaWithVersion = client.getLatestSchemaVersion(group, null);
+        assertEquals(schemaWithVersion.getVersionInfo(), version3);
+
+        // add the schema again. it should get a new version
+        VersionInfo version4 = client.addSchema(group, schemaInfo2);
+        assertEquals(version4.getOrdinal(), 3);
+        assertEquals(version4.getVersion(), 2);
+    }
+
+    abstract SchemaStore getStore();
+
+}
+
